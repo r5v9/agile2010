8488449cb11da13d9d60a909955f23dcaf334231
Julio Maia <julio.maia@gmail.com>
2010-07-15 15:45:43 +1000

    added watch script to rebuild index.html on changes to source files

---
+++ watch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+files_to_watch="index.erb topics.yml speakers.yml"
+
+old_timestamp=`ls -lT $files_to_watch`
+
+while [ true ]; do
+	current_timestamp=`ls -lT $files_to_watch`
+	
+	if [[ "$current_timestamp" != "$old_timestamp" ]]; then
+		echo `date` "started rebuilding"
+		ruby build-html.rb
+		echo `date` "finished rebuilding"
+		old_timestamp="$current_timestamp"
+	fi
+	
+	sleep 1
+done
